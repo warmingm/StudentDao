9b246a6c0dd948736aa14f1fc47a401972ee9a97
warmingm <warmingm@1162663065@qq.com>
2022-03-20 17:02:59 +0800

    remove MyFirstMQ

--- MyFirstMQ/src/ConsumeClient.java
+++
@@ -1,7 +0,0 @@
-public class ConsumeClient {
-    public static void main(String[] args) throws Exception{
-        MqClient client = new MqClient();
-        String message = client.consume();
-        System.out.println("获取的消息为：" + message);
-    }
-}
--- MyFirstMQ/src/ProduceClient.java
+++
@@ -1,6 +0,0 @@
-public class ProduceClient {
-    public static void main(String[] args) throws Exception{
-        MqClient client = new MqClient();
-        client.produce("第二条消息");
-    }
-}
